5e57b3f605e49e6af6bb17d8a77bf69de8962ad1
Nicholas Conn <188623nc@gmail.com>
2023-10-03 16:15:16 -0400

    Autopep8 formatting
    just fixed indentations to fit pythonic standards

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import socketserver
-import threading 
+import threading
 from datetime import datetime
-import json 
+import json
 
 
 # Versioning 
@@ -9,72 +9,59 @@ import json
 
 
 class Client:
-    def __init__(self, client_version, profile_hash): # Profile hash can be anything until I develop a hashing method  
-        self.client_version = client_version # The client version 
-        self.phash = profile_hash # Just going to be a name or something 
+    def __init__(self, client_version, profile_hash):  # Profile hash can be anything until I develop a hashing method
+        self.client_version = client_version  # The client version
+        self.phash = profile_hash  # Just going to be a name or something
         self.connected = datetime.now()
         self.messages = {}
-        self.token = None # Need to implement a token 
+        self.token = None  # Need to implement a token
 
-    
     def readMessage(self, message):
         message = json.loads(message)
         t = message.pop('token')
         n = message.pop('n')
-         
+
         self.messages[n] = message
         return message['content']
-    
-
-
-
-    
-    
-    
-
 
 
 class Server:
     def __init__(self):
         self.connections = []
         self.server_version = "0.0.0.1"
-        self.server_hash = None # Need to implement unique server hashes (easy way for server identification)
-        self.verification_hash = None # Need to implement unique verification hashes for the server 
-        self.messages = {} # This will be a database in the full version 
-    
-    
+        self.server_hash = None  # Need to implement unique server hashes (easy way for server identification)
+        self.verification_hash = None  # Need to implement unique verification hashes for the server
+        self.messages = {}  # This will be a database in the full version
+
     # Authorize access to the server 
     def authorize(self, user):
-        message = {} # holds the auth message
+        message = {}  # holds the auth message
         message["version"] = self.server_version
-        message["token"]  = None # Need to implement a token system or session system 
+        message["token"] = None  # Need to implement a token system or session system
 
-        return message 
+        return message
 
     def saveMessages(self, sent, user):
         if user in self.messages.keys():
             self.messages[user] = {**self.messages[user], **sent}
-        
-        else: 
+
+        else:
             self.messages[user] = sent
-    
+
     def loadMessages(self, user):
         if user not in self.messages.keys():
             return 1
 
-        long_string = ">>>  " # Combine all messages into 1 long string (for now )
+        long_string = ">>>  "  # Combine all messages into 1 long string (for now )
         user_messages = self.messages[user]
         for message in user_messages.keys():
             long_string += user_messages[message]['content'] + "\n" + ">>>  "
-        
+
         return long_string
-            
-        
-            
-                
-    
+
+
 opencord_server = Server()
-        
+
 
 class TCPHandler(socketserver.BaseRequestHandler):
 
@@ -84,7 +71,7 @@ class TCPHandler(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print(f"{self.client_address[0]} wrote: {self.data}")
         self.request.sendall(self.data.upper())
-        while True: 
+        while True:
             print(f"Socket: {server.socket._closed}")
             # self.data = self.request.recv(1024).strip()
             self.data = self.request.recv(1024)
@@ -96,20 +83,20 @@ class TCPHandler(socketserver.BaseRequestHandler):
             message = message.decode('utf-8')
             new_message = "Server says: " + message.upper() + '\n'
             new_message = bytes(new_message, 'utf-8')
-         
+
             print(f"{self.client_address[0]} wrote: {message}")
             self.request.sendall(new_message)
-        
+
 
 class ThreadedTCPHandler(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         message = self.data.strip()
         message = self.data.decode('utf-8')
         msg_object = json.loads(message)
         print(f"{self.client_address[0]} wrote: {self.data}")
-        
+
         client = Client("0.0.0.1", msg_object['profile'])
 
         print(f"Client address: {self.client_address}")
@@ -122,8 +109,8 @@ class ThreadedTCPHandler(socketserver.BaseRequestHandler):
         loaded_messages = opencord_server.loadMessages(client.phash)
         if loaded_messages != 1:
             self.request.sendall(bytes(loaded_messages, 'utf-8'))
-            
-        while True: 
+
+        while True:
             try:
                 # print(f"Socket: {server.socket._closed}")
                 # self.data = self.request.recv(1024).strip()
@@ -140,20 +127,14 @@ class ThreadedTCPHandler(socketserver.BaseRequestHandler):
 
                 new_message = "Server says: " + m.upper() + '\n'
                 new_message = bytes(m, 'utf-8')
-         
+
                 print(f"{self.client_address[0]}: {message}")
 
-                
                 self.request.sendall(new_message)
             except Exception as e:
                 print(f"Error: {e}")
                 opencord_server.saveMessages(client.messages, client.phash)
                 break
-                
-                
-                
-                
-                
 
 
 # The class should already be defined but we can override functions inside the class
@@ -171,14 +152,13 @@ if __name__ == '__main__':
     #     # print(f"Data: {server.fileno()}")
     #     server.serve_forever()
 
-
-    # Most of this code is from the docs with edits made to it 
+    # Most of this code is from the docs with edits made to it
     # https://docs.python.org/3/library/socketserver.html
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPHandler)
-    
+
     with server:
         ip, port = server.server_address
-        
+
         # Start a thread with the server -- that thread will then start
         # one more thread for each request 
         server_thread = threading.Thread(target=server.serve_forever)  # This is the serving thread
@@ -188,25 +168,21 @@ if __name__ == '__main__':
         # So they are abruptly stopped at shutdown meaning resources may not be released properly. 
         # For threads to stop gracefully, they should be non-daemonic and use a signalling mechanism
         # such as an Event.
-        server_thread.daemon = True 
-        
+        server_thread.daemon = True
+
         server_thread.start()
-        
-        
+
         print(f"Server loop running in thread {server_thread.name}")
-        while True: 
+        while True:
             try:
                 t = input("\"Exit\" to stop the server: ")
                 if t == "Exit":
                     break
-        
+
                 print("Shutting Down")
                 server.shutdown()
             except Exception as e:
                 print(f"Error Here: {e}")
                 break
-        
-        server.shutdown() 
-            
 
-    
+        server.shutdown()
